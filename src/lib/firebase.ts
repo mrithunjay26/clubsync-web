9e4f489f8cb97dde22be2f6157c822f8a588e90f
mrithunjay26 <mrithunjayshanmuganand@gmail.com>
2026-06-13 21:19:07 -0700

    fixed vercel deployment issues with firebaseconfig

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,19 @@
-// import { initializeApp, getApps } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { getDatabase } from 'firebase/database';
-//
-//
-// // const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-// export const auth = getAuth(app);
-// export const db = getDatabase(app);
-// export default app;
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "asdgsdgasdgasdgasdfasdgasdg",
+  authDomain: "asdgafsdgsadgasfg-asdfsadf.firebaseapp.com",
+  databaseURL: "https://asdgsadgasdgasdg-cbf2d-default-rtdb.firebaseio.com",
+  projectId: "asfgasdgasdg-asdgfsag",
+  storageBucket: "asgdgsdgasdg-cbf2d.appspot.com",
+  messagingSenderId: "asdfsadfasdf",
+  appId: "1:sdfasfsaf:web:0f76ac882b49f38cef923d",
+  measurementId: "G-sdasdafdsa"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export default app;
