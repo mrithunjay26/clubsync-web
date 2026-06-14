57aa0b26d81b5082cd0e9c69e0900980643d6686
mrithunjay26 <mrithunjayshanmuganand@gmail.com>
2026-06-13 21:23:41 -0700

    updated package.json, deleted vercel-build over build errors

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { User, onAuthStateChanged } from 'firebase/auth';
-// import { auth } from '@/lib/firebase';
+import { auth } from '@/lib/firebase';
 
 interface AuthContextType {
   user: User | null;
